33ed169d1913660478a63143a7afba11f5ca0ace
Jonathan Delgado <jonathandelgado0327@gmail.com>
2018-12-27 12:43:14 -0500

    Removed older files outside of folder structure cached in repo.

--- benchmark.h
+++
@@ -1,6 +0,0 @@
-//General test program
-//Returns the time elapsed for the benchmark
-float benchmarkGeneral(void (*test)(void));
-//Runs the general test program several times, then prints the average time run
-//Should be a more accurate representation of time
-void benchmarkGeneralMulti(void (*test)(void), int runs);
--- makefile
+++
@@ -1,8 +0,0 @@
-CC = gcc
-flags = -Wall -g -o
-
-matrix: main.o matrix.o vector.o benchmark.o file.o matrix_reader.o dict.o
-	$(CC) $(flags) matrix main.o matrix.o vector.o benchmark.o file.o matrix_reader.o dict.o
-
-clean:
-	rm *.o
