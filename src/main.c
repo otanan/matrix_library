b15bd2f42e9bf8977d14c29da047c7e4afb8f844
Jonathan Delgado <jonathandelgado0327@gmail.com>
2019-01-05 15:24:32 -0500

    Removed to-do list for better readability of what main.c is used for. Considering moving the to-do list to a different public area

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
+/*
+*Filename: 		main.c
+*Author: 		Jonathan Delgado
+*Description: 	Main source of entry for the program,
+*				especially for debugging
+*/
+
 #include "../include/benchmark.h"
 
 #ifndef DICT_H
@@ -7,44 +14,8 @@
 
 #include "../include/matrix_reader.h"
 
-//TODO
-//Considered implementing cutoff time for benchmarks
-
-//Potential init function in constructor solely for pointing to methods?
-	//use case example is in alternative constructors, or finding way to all link to the same base constructor
-
-//Improve efficiency of toMatrix and toVector since they currently use the default constructor
-	//which initializes everything to 0 before initializing arg
-	//Consider changing calloc/malloc to check whether we're creating a new object vs converting one
-
-//Improve equality command syntax (rewriting commands)
-	//Dictionary connecting labels to types labelDict(label, "matrix")
-	//matrix_dict
-	//vectorDict
-
-//Changing identities is an issue
-	//Consider resolving by making identity matrices constants, and returning
-	//a pointer? or something along those lines
-
-//Possible redundancy in memory?
-//Getting copies arrays, setting copies arrays
-
-//array.c
-	//Holds general functions such as: sum, scale, copy, multiply, etc
-	//Specialized functions for dynamic behavior: grow,
-
-//Matrix Reader
-	//Organize functions in sections and names
-
-//Cut rows, cut cols helper functions
-
-//Add error checking to matrix_reader
-
-//Understand success/failure program flags
-
 void matrix_reader_test(void);
 void matrix_reader_test() {
-	//Read file belongs to file.h, implementation is currently not working properly
 	FILE *fp = fopen("res/input.txt", "r");
 	read_matrix_file(fp);
 
